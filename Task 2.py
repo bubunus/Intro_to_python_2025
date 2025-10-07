a5c77219d05776cbfb7b9a4a33ec8c5dd4df6ab7
Damir <110835757+bubunus@users.noreply.github.com>
2025-10-07 12:00:09 +0200

    Delete Task 2.py

--- Task 2.py
+++
@@ -1,9 +0,0 @@
-year = int(input("Enter a year: "))
-if year % 4 != 0:
-    print("not a leap year")
-elif year % 100 != 0:
-    print("is a leap year")
-elif year % 400 != 0:
-    print("not a leap year")
-else:
-    print("is a leap year")
