ee5924cf7fedf6428b24ece79bf52562098e56d0
Damir <110835757+bubunus@users.noreply.github.com>
2025-10-07 12:00:41 +0200

    Delete Task3.py

--- Task3.py
+++
@@ -1,7 +0,0 @@
-sentence = input("Enter a sentence: ")
-letter = input("Enter a letter: ")
-count = 0
-for ch in sentence:
-    if ch == letter:
-        count += 1
-print("The letter", letter, "appears", count, "times.")
